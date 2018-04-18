88668a4bf970345dd7096a513298aeece75e10f9
ahy3nz <alexander.h.yang@vanderbilt.edu>
2018-04-18 11:21:03 -0500

    Cerns-head-notail

---
+++ atomistic/cerns-head-notail/change-bond.py
@@ -0,0 +1,14 @@
+import numpy as np
+
+
+to_delete = range(5, 11) + range(23, 33)
+angles_to_print = []
+angles = np.loadtxt('bonds.txt', dtype='int')
+for atype, ai, aj in angles:
+    if ai not in to_delete and aj not in to_delete:
+        angles_to_print.append([atype, ai, aj])
+        for i, indices in enumerate(angles_to_print[-1][1:]):
+            if indices >= 11:
+                angles_to_print[-1][i+1] -= 6
+
+np.savetxt('bonds-new.txt', np.asarray(angles_to_print), fmt='%d')
