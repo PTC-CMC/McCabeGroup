44eaea0673c50a4a63011e424a7d44e7f2946115
Alex Yang <ahy3nz@virginia.edu>
2018-05-01 17:21:22 -0500

    More water renaming

--- atomistic/tip3p/tip3p.py
+++ atomistic/tip3p/tip3p.py
@@ -1,5 +1,5 @@
 import mbuild as mb
-class HOH(mb.Compound):
+class SOL(mb.Compound):
     def __init__(self):
-        super(HOH,self).__init__()
+        super(SOL,self).__init__()
         mb.load('tip3p.mol2', compound=self, relative_to_module=self.__module__)
