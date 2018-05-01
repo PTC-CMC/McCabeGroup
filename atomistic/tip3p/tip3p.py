ba46511a15c4d5600e8da626f11dc0b8b0b024e8
Alex Yang <ahy3nz@virginia.edu>
2018-05-01 17:21:52 -0500

    clear out old tip3p files

--- atomistic/tip3p/tip3p.py
+++
@@ -1,5 +0,0 @@
-import mbuild as mb
-class SOL(mb.Compound):
-    def __init__(self):
-        super(SOL,self).__init__()
-        mb.load('tip3p.mol2', compound=self, relative_to_module=self.__module__)
