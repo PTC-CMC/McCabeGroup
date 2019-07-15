90656bd6a2e79d1b195f568f5a05b991ee47ff86
Alex Yang <ahy3nz@virginia.edu>
2019-07-15 11:10:48 -0500

    Correct alignment

--- atomistic/c18ffa/itp_to_cmpd.py
+++ atomistic/c18ffa/itp_to_cmpd.py
@@ -14,7 +14,7 @@ ref_molecule = mb.load('ffa18.mol2')
 
 cmpd = itp_utils.compound_from_itp('ffa18.itp')
 cmpd = itp_utils.coordinates_from_compound(ref_molecule, cmpd)
-aligned_cmpd = xml_utils.align_cmpd(cmpd, [17,0])
+aligned_cmpd = xml_utils.align_cmpd(cmpd, [3,17])
 
 aligned_cmpd.save('{}.mol2'.format(aligned_cmpd.name), 
         residues=[aligned_cmpd.name], overwrite=True)
