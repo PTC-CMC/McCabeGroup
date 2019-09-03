4071255eddf98f63f785238ca893e1201c0b6678
Alex Yang <ahy3nz@virginia.edu>
2019-09-03 12:43:27 -0500

    CG ffa16

---
+++ cg/prototypes/ffa16.py
@@ -0,0 +1,7 @@
+import mbuild as mb
+class ffa16(mb.Compound):
+    def __init__(self):
+        super(ffa16,self).__init__()
+        mb.load('ffa16.mol2', compound=self, relative_to_module=self.__module__)
+        for part in self.particles():
+            part.name = "_" + part.name
